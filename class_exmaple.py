e4522278bc03a6b45a5e456c764c38cb6969868a
DESKTOP-UVF4DDQ\user <kdseo23025@gmail.com>
2021-11-19 14:27:12 +0900

    class person edited. Multiple attributes

--- class_exmaple.py
+++ class_exmaple.py
@@ -1,14 +1,26 @@
 class person:
+    def __init__(self, name, age, address):
+        self.hello = '안녕하세요'
+        self.name = name
+        self.age = age
+        self.address = address
+
     def greeting(self):
-        print('Hello')
+        # print('Hello')
+        # print(self.hello)
+        print('{0} 저는 {1}입니다. 나이는 {2}이고, 주소는 {3}입니다.'.format(self.hello, self.name, self.age, self.address))
 
-    def hello(self):
-        self.greeting()
+    # def hello(self):
+    #     self.greeting()
 
-james = person()
-james.greeting()
-james.hello()
-isinstance(james, person)
+# james = person()
+# james.greeting() # >>> 안녕하세요
+# james.hello()
+# isinstance(james, person)
+
+maria = person('마리아', 30, '서울시 서초구 반포동')
+maria.greeting()
+
 
 def factorial(n):
     if not isinstance(n, int) or n < 0: # n이 정수가 아니거나 음수이면 함수를 끝냄
