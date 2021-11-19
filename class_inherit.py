b6ba1a0fc0a75e987a2992d5bac51d0d65dd2bda
DESKTOP-UVF4DDQ\user <kdseo23025@gmail.com>
2021-11-19 16:53:43 +0900

    class inheritance

---
+++ class_inherit.py
@@ -0,0 +1,22 @@
+class Person:
+    def greeting(self):
+        print('안녕하세요.')
+
+
+class Student(Person):
+    def study(self):
+        print('공부하기')
+
+
+class PersonList:
+    def __init__(self):
+        self.person_list = [] # 리스트 속성에 Person 인스턴스를 넣어서 관리
+
+    def append_person(self, person): # 리스트 속성에 Person 인스턴스를 추가하는 함수
+        self.person_list.append(person)
+
+
+james = Student()
+james.greeting()  # 안녕하세요.: 기반 클래스 Person의 메서드 호출
+james.study()  # 공부하기: 파생 클래스 Student에 추가한 study 메서드
+print(issubclass(Student, Person))
