6bb1758466c654cf7b403693b54d292ffe553d89
DESKTOP-UVF4DDQ\user <kdseo23025@gmail.com>
2021-11-19 13:44:06 +0900

    Add contents

--- class_exmaple.py
+++ class_exmaple.py
@@ -2,5 +2,17 @@ class person:
     def greeting(self):
         print('Hello')
 
+    def hello(self):
+        self.greeting()
+
 james = person()
 james.greeting()
+james.hello()
+isinstance(james, person)
+
+def factorial(n):
+    if not isinstance(n, int) or n < 0: # n이 정수가 아니거나 음수이면 함수를 끝냄
+        return None
+    if n == 1:
+        return 1
+    return n * factorial(n - 1)
